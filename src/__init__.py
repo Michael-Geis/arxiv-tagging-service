6c1503f15fca060de8e9570919b3e884810d8fac
Michael Geis <michael.leamy.geis@gmail.com>
2024-12-16 22:43:24 -0600

    adds .env file and default values for watched and target dirs, adds dotenv

--- src/__init__.py
+++ src/__init__.py
@@ -3,18 +3,21 @@ import win32event
 import servicemanager
 import logging
 from watchdog.observers import Observer
-from watchdog.observers.api import EventQueue
 from watchdog.events import FileSystemEventHandler
 import os
 import shutil
 import time
-from main import get_arxiv_metadata
 import arxiv
+from dotenv import load_dotenv
 
 logging.basicConfig(
     level=logging.INFO,  # Set the logging level (e.g., DEBUG, INFO, WARNING)
     format="%(asctime)s - %(levelname)s - %(message)s"  # Define the log message format
 )
+
+load_dotenv()
+for k,v in os.environ.items():
+    os.environ[k] = os.path.expandvars(v)
 
 class ArXivTagger:
     
@@ -43,8 +46,8 @@ class FileDownloadHandler(FileSystemEventHandler):
         super().__init__()
         self.pending_crdownload_files = {}
         self.timeout_threshold = 1.0
-        self.client = arxiv.Client()
-        self.tagger = ArXivTagger(destination_dir='C:\\Users\\Leems\\Desktop\\test-dir')
+        self.client = arxiv.Client() # Does arxiv provide an api to pass in a logger?
+        self.arxiv_tagger = ArXivTagger(destination_dir=os.getenv('target_dir'))
     
     def on_created(self,event):
         if not event.is_directory and event.src_path.endswith('.crdownload'):
@@ -54,37 +57,58 @@ class FileDownloadHandler(FileSystemEventHandler):
         if not event.is_directory and event.src_path in self.pending_crdownload_files:
             self.download_complete_callback(event.dest_path)
             del self.pending_crdownload_files[event.src_path]
-
         
     def download_complete_callback(self,filepath):
-        logging.info(f'Download complete. Downloaded file path: {filepath}')
+
+        logging.info(f'Download completed at: {filepath}')
         if not filepath.endswith('.pdf'):
             return None
-        
         self._handle_pdf_download(filepath) 
     
     def _handle_pdf_download(self,filepath):
 
-        results = get_arxiv_metadata(
-            filepath = filepath,
-            client=self.client
-        )
-
-        if not results:
+        metadata = self._get_arxiv_metadata(filepath=filepath)
+        if not metadata:
             logging.info(f'Downloaded file: {filepath} was not found on the arxiv')
             return None
-        
-        logging.info(f'arxiv metadata found: {results}')
+        logging.info(f'arxiv metadata found: {metadata}')
         logging.info(f'renaming and moving...')
-        self.tagger.rename(filepath,metadata=results)
+        self.arxiv_tagger.rename(source_path=filepath,metadata=metadata)
 
     def _cleanup_stale_crdownload_files(self):
+
         current_time = time.time()
         self.pending_crdownload_files = {
             path: timestamp
             for path, timestamp in self.pending_crdownload_files.items()
             if current_time - timestamp <= self.timeout_threshold
         }
+    
+    def _get_arxiv_metadata(self,filepath) -> dict[str,object] | None:
+        """Takes a file path and returns the paper metadata if it is an arxiv paper. Otherwise, returns None
+
+        Args:
+            filepath (str): Path to the file to check
+            client (arxiv.Client): Client used to query the arxiv api
+
+        Returns:
+            obj: Either None, if the file name does not correspond to an arxiv article, or the metadata of the paper.
+        """
+        
+        # The basename will always correspond to an arxiv id if it is an arxiv paper
+
+        file_name = os.path.splitext(os.path.basename(filepath))[0]
+        search = arxiv.Search(id_list=[file_name])
+        try:
+            result = next(self.client.results(search=search))
+            logging.info(f'Found arXiv article corresponding to path: "{filepath}".')
+        except StopIteration:
+            logging.info(f'No results returned for path: "{filepath}". No action taken.') 
+            return None 
+        author_list = [author.name for author in result.authors]
+
+        ## Could these attributes be missing?
+        return {'title': result.title, 'author_list': author_list, 'primary_category':result.primary_category}
             
 
 class ArxivTaggingService(win32serviceutil.ServiceFramework):
@@ -110,21 +134,16 @@ class ArxivTaggingService(win32serviceutil.ServiceFramework):
     def main(self) -> None:
         pass
 
-if __name__ == "__main__":
-    logging.info("app start")
+logging.info("app start")
+observer = Observer()
+handler=FileDownloadHandler()
+SOURCE_DIR = os.getenv('SOURCE_DIR')
+observer.schedule(event_handler=handler,path=SOURCE_DIR,recursive=False)
 
-    observer = Observer()
-    handler=FileDownloadHandler()
-    path = "C:\\Users\\Leems\\desktop"
-    observer.schedule(event_handler=handler,path=path,recursive=False)
-    
-    logging.info(f'monitoring changes in dir {path}')
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-
-
-    ## TODO: Handle the fact that downloads create tmp files and rename them.
+logging.info(f'monitoring changes in dir {SOURCE_DIR}')
+observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
